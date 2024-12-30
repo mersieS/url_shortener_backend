faa4f383ab3a3f9029006ae836b063856a7410f5
Salo <salihimranbuker44@gmail.com>
2024-12-30 11:42:17 +0300

    Fix encrypt

--- config/initializers/encryption.rb
+++ config/initializers/encryption.rb
@@ -1,3 +1,4 @@
 Rails.application.config.active_record.encryption = {
-  key_derivation_salt: ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT']
+  key_derivation_salt: ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT'],
+  key_base: ENV['ACTIVE_RECORD_ENCRYPTION_KEY_BASE']
 }
