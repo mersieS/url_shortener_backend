4c40eff285805f5b8aecd1bf3c5fd3fd66e6a73e
Salo <salihimranbuker44@gmail.com>
2024-12-30 11:50:36 +0300

    Hotfix

--- config/initializers/encryption.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.active_record.encryption = {
-  key_derivation_salt: ENV['ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT'],
-  key_base: ENV['ACTIVE_RECORD_ENCRYPTION_KEY_BASE']
-}
