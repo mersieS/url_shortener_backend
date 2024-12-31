0c1ca8e8123240c5c5c43ef8bf548c553b5fed7c
Salo <salihimranbuker44@gmail.com>
2024-12-31 13:52:09 +0300

    Url uniqueness scope by user

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,7 @@ class Url < ApplicationRecord
   belongs_to :user
   has_many :clicks, dependent: :destroy
 
-  validates :original_url, presence: true, uniqueness: true,  format: URI.regexp(%w[http https])
+  validates :original_url, presence: true, uniqueness: { scope: :user_id, message: "has already been taken for this user" }, format: URI.regexp(%w[http https])
   validates :name, presence: true
 
   before_create :generate_short_url
